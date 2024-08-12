806422950c41eb451d3351f6cc37ff308b1554d6
msz47 <139070715+msz47@users.noreply.github.com>
2024-08-12 13:59:48 +0500

    Create bundle-box2.js

---
+++ bundle-box2.js
@@ -0,0 +1,29 @@
+const header = document.querySelector('.header');
+const bundleBoxBtn = document.getElementById('bundle-box-div');
+if(header && !bundleBoxBtn){
+    // Create a new div element with id 'bundle-box-div'
+  const div = document.createElement('div');
+  div.id = 'bundle-box-div';
+  
+  const button = document.createElement('a');
+  button.id = 'bundle-box-btn';
+  button.textContent = 'Bundle box';
+  button.href = '/apps/myapp?routeAction=bundleBox';
+
+  button.style.padding = '10px 25px';
+  button.style.borderRadius = '5px';
+  button.style.backgroundColor = '#008060';
+  button.style.color = 'white';
+  button.style.border = 'none';
+  button.style.textDecoration = 'none';
+  button.style.display = 'inline-block';
+  
+  button.addEventListener('mouseover', function() {
+      button.style.backgroundColor = '#006c4f'; 
+  });
+  button.addEventListener('mouseout', function() {
+      button.style.backgroundColor = '#008060'; 
+  });
+  div.appendChild(button);
+  header.appendChild(div);
+}
